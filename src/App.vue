e4464229cdad2bafb7d0b973f70fc72b07998c89
Natasa <natasa.t.markovic@gmail.com>
2024-01-25 23:20:47 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>Natasa Reaction Timer</h1>
+ <button @click="start" :disabled="isPlaying">play</button>
+ <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+ <Results v-if="showResults" :score="score" />
+ <!-- <p v-if="showResults">Reaction time: {{ score }}</p> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Results
+  },
+  data(){
+    return {
+      isPlaying: false,
+    delay: null,
+    score: null,
+    showResults: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+      
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +46,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="block">Reaction time: {{ score }}</div>
+<p>Rank: {{ rank }}</p>
+</template>
+
+<script>
+export default{
+props: ['score'],
+data(){
+    return {
+        rank: null
+    }
+},
+mounted(){
+    if(this.score < 250){
+        this.rank = 'Ninja fingers'
+    } else if(this.score < 400){
+        this.rank = 'Rapid Reflexes'
+    }else{
+        this.rank = "Snail pace"
+    }
+}
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
